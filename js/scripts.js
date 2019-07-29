28d002497f44e41c71b9ca29675e80e2f989ee2c
mohamed hashi <kidnoodle12@gmail.com>
2019-07-29 11:09:19 +0300

    the data has been printed on the screen

--- js/scripts.js
+++ js/scripts.js
@@ -5,16 +5,32 @@ $(document).ready(function(){
 
     $("#output1").text(window.selectedsize)
     if (window.selectedsize === "big"){
-      var money_size = 30
+    window.money_size = 30
     }
     else if (window.selectedsize === "small") {
-      var money_size = 10;
+       window.money_size = 10;
 
     }
     else if(window.selectedsize === "medium"){
-      var money_size= 20;
+      window.money_size= 20;
     }
-    alert(money_size);
+    alert(window.money_size);
+
+  });
+  $("select#crust").change(function(){
+    window.selectedcrust = $(this).children("option:selected").val();
+    $("#output3").text(window.selectedcrust)
+    if (window.selectedcrust === "crispy"){
+      window.money_crust = 30
+    }
+    else if (window.selectedcrust === "stuff") {
+      window.money_crust = 10;
+
+    }
+    else if(window.selectedcrust === "Gluten-free"){
+      window.money_crust = 20;
+    }
+    alert(window.money_crust);
 
   });
 
@@ -22,32 +38,20 @@ $(document).ready(function(){
      window.selectedtopping = $(this).children("option:selected").val();
     $("#output2").text(window.selectedtopping)
     if (window.selectedtopping === "pineapple"){
-      var money_topping = 30
+      window.money_topping = 30
     }
     else if (window.selectedtopping === "chicken") {
-      var money_topping = 10;
+      window.money_topping = 10;
 
     }
     else if(window.selectedtopping === "cheese"){
-      var money_topping = 20;
+      window.money_topping = 20;
     }
-    alert(money_topping);
+    alert(window.money_topping);
+    var total = window.money_crust + window.money_size + window.money_topping
+    $("#output4").text(total)
   });
-  $("select#crust").change(function(){
-    window.selectedcrust = $(this).children("option:selected").val();
-    $("#output3").text(window.selectedcrust)
-    if (window.selectedcrust === "crispy"){
-      var money_crust = 30
-    }
-    else if (window.selectedcrust === "stuff") {
-      var money_crust = 10;
 
-    }
-    else if(window.selectedcrust === "gluten-free"){
-      var money_crust = 20;
-    }
-    alert(money_crust);
-  });
 
 
 });
